def02b24da75c81d1e29d9bde68a33e7f07df703
Mohamad Alamin <alamin.mohamad.a0@gmail.com>
2023-04-24 12:03:22 +0200

    Added a favicon to the quiz page

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -12,3 +12,10 @@ export const createWelcomeElement = () => {
   `;
   return element;
 };
+let favicon = document.querySelector("link[rel~='icon']");
+if (!favicon) {
+    favicon = document.createElement('link');
+    favicon.rel = 'icon';
+    document.head.appendChild(favicon);
+}
+favicon.href = 'favicon.png'
