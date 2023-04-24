c61a5954c76118dd267f48b5d8619bdfc13e4096
Ethel Chila <chilachila016@gmail.com>
2023-04-24 13:59:10 +0200

    made it possible for user to select an answer

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -7,5 +7,18 @@ export const createAnswerElement = (key, answerText) => {
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
+  element.addEventListener('click', () => {
+    const answerEl = document.querySelectorAll("li")
+    answerEl.forEach((el) => {
+el.style.fontWeight = ""
+      el.style.backgroundColor = ""
+    }) 
+    element.style.fontWeight = "bold"
+    element.style.backgroundColor = "#bde0fe"
+    
+     const event = new CustomEvent('answerSelected', { detail: { key, answerText } });
+    element.dispatchEvent(event); 
+})
   return element;
 };
+
