7f33dfd8a25a88ede88e5ac8fc54957b3636b7b2
Mohamad Alamin <alamin.mohamad.a0@gmail.com>
2023-04-25 23:27:30 +0200

    highlighted (green) the correct answer when selected and (red) for the wrong answers

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -2,23 +2,27 @@
  * Create an Answer element
  * @returns {Element}
  */
-export const createAnswerElement = (key, answerText) => {
+
+export const createAnswerElement = (key, answerText, correct) => {
   const element = document.createElement('li');
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
   element.addEventListener('click', () => {
-    const answerEl = document.querySelectorAll("li")
+    const answerEl = document.querySelectorAll('li');
     answerEl.forEach((el) => {
-el.style.fontWeight = ""
-      el.style.backgroundColor = ""
-    }) 
-    element.style.fontWeight = "bold"
-    element.style.backgroundColor = "#bde0fe"
-    
-     const event = new CustomEvent('answerSelected', { detail: { key, answerText } });
-    element.dispatchEvent(event); 
-})
+      el.style.fontWeight = '';
+      el.style.backgroundColor = '';
+    });
+    element.style.fontWeight = 'bold';
+    const score = 0;
+    if (key === correct) {
+      element.style.backgroundColor = 'green';
+      score++;
+    } else {
+      element.style.backgroundColor = 'red';
+    }
+  });
+
   return element;
 };
-
