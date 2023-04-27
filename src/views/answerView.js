da5f6cb8a79a1ef8ddcb5c59c9913a3405ff3c14
Samrawit Girma <samrawitg39333.eg@gmail.com>
2023-04-27 14:04:55 +0200

    update score as they select answers

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -1,37 +1,66 @@
+   
+  const questionsNum = 10;
+  let score = 0;
+  let answerSelected = false;
+  
+
+
+
+
 /**
  * Create an Answer element
  * @returns {Element}
  */
 
-let score = 0;
-let answerSelected = false;
-let questionsNum = 10;
+
+
 
 export const createAnswerElement = (key, answerText, correct) => {
   const element = document.createElement('li');
   element.innerHTML = String.raw`
     ${key}: ${answerText}
   `;
+  
+  answerSelected = false;
+
+
+  const scoreElement = document.getElementById('score');
+  scoreElement.innerHTML = `Score: ${score}/${questionsNum}`;
 
   element.addEventListener('click', () => {
-    if (answerSelected) {
-      return;
-    }
-    answerSelected = true;
-    if (key === correct) {
+    
+    if (!answerSelected) {
+      const answerEls = document.querySelectorAll('li');
+      answerEls.forEach((el)=> {
+        if(el.getAttribute('dataSet') !== correct) {
+          el.style.pointerEvents = 'none';
+        }
+        el.style.fontWeight = '';
+        el.style.backgroundColor = '';  
+      });
+
+      element.style.fontWeight = 'bold';   
+
+
+
+       if (key === correct) {
       score++;
       element.style.backgroundColor = 'green';
+  
+      
     } else {
       element.style.backgroundColor = 'red';
+
       if (document.querySelector(`li[dataSet="${correct}"]`)) {
-        document.querySelector(
-          `li[dataSet="${correct}"]`
-        ).style.backgroundColor = 'green';
+        document.querySelector( `li[dataSet="${correct}"]` ).style.backgroundColor = 'green';
+        
       }
     }
-    const scoreElement = document.getElementById('score');
+    answerSelected = true;
+    
     scoreElement.innerHTML = `Score: ${score}/${questionsNum}`;
-    answerSelected = false;
+  }
+
   });
  
   element.setAttribute('dataSet', key);
